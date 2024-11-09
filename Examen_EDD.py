3c26539f20cf330120a4d1dea52af0570a32e34b
Luis Cabello <lcabellocabanillas@safareyes.es>
2024-11-09 12:37:43 +0100

    Unión del PseInt al phyton

--- Examen_EDD.py
+++ Examen_EDD.py
@@ -67,7 +67,32 @@ elif + else posterior.
 # Si el número escogido es menor que 100 el descuento es del 10% sobre el total de la compra, si es mayor o igual a 100 el descuento es del 20%. Además, si el número es par,  
 # se le aplicará un 2% adicional. Por el contrario, si es impar, se le aplicará un 3%.
 """
+Para comprobar el funcionamiento de este código en el archivo PseInt Ejercicios está el mismo código, que hay a continuación, y si en PseInt abres el archivo lo podrás comprobar
+Algoritmo Examen
 
+	Escribir "Hoy es tu día puedes llevarte hasta un 23% de descuento en tu compra total para saber cual es tu % de descuento debes ingresar tu coste de la compra TOTAL para saber a cuanto te saldrá."
+	Escribir "El juego consiste en que se sacará un número al azar entre el 0 y el 500 si el número escogido es menor que 100 el descuento es del 10%, si es mayor o igual a 100 el descuento es del 20%. Además, si el número es par,  se le aplicará un 2% adicional. Por el contrario, si es impar, se le aplicará un 3%"
+	Numeroazar=Aleatorio(0,500)
+	descuento_final=0
+	NumPar=PAR
+	Escribir  "El número que salió fué el " Numeroazar
+	si Numeroazar >100
+		descuento_final= descuento_final+20
+	SiNo
+		descuento_final= descuento_final+10
+	FinSi
+	si Numeroazar%2=0
+		descuento_final= descuento_final+2
+	SiNo
+		descuento_final= descuento_final+3
+	FinSi
+	Escribir "Su descuento es de " descuento_final "%"
+	Leer dineroTotal
+	Escribir dineroTotal
+	Escribir "El descuento es de " (dineroTotal*descuento_final)/100 "$"
+	Escribir "El dinero a pagar es " dineroTotal-(dineroTotal*descuento_final)/100 "$"
+	
+FinAlgoritmo
 """
 
 #Actividad 7. Pseint (1 punto) Un colegio desea saber qué porcentaje de niños y qué porcentaje de niñas hay en el curso actual. Además, queremos saber si el porcentaje es par o impar.
