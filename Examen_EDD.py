6d8dbc9082acacc1b8c23908c3b09631e1a3e528
Luis Cabello <lcabellocabanillas@safareyes.es>
2024-11-10 18:02:14 +0100

    ejercicio 9

--- Examen_EDD.py
+++ Examen_EDD.py
@@ -147,23 +147,36 @@ Escribir "El director de la escuela necesita saber cuanto porcentaje de niños y
 # el usuario ingresa un número para el cual la suma de sus dígitos sea mayor que 1000 ó múltiplo de 5. Finalmente, mostrar cuántos números impares ingresó el usuario antes de cortar la 
 # repetición. Explica el proceso completo de depuración para el siguiente código, incluyendo la inspección de variables desde el inicio hasta el final del proceso.
 
-def esPar(numero):
-    return numero%3==0
+def esImpar(numero):
+    return numero%3 == 0
 
 def sumatoriaDigitos(numero):
-    total=0
-    while total !=0:
-        ultimoDigito=numero%10
-        total=total+ultimoDigito
-        numero=numero/10
+    total = 0
+    while numero > 0:
+        ultimoDigito = numero%10
+        total = total + ultimoDigito
+        numero = numero//10
     return total
 
-cantidadImpares=0
-n=int(input("Escribe un número:"))
-while sumatoriaDigitos(n)>1000 and sumatoriaDigitos(n)%3!=0:
-    if esPar(n):
-        cantidadImpares=cantidadImpares+1
-    n=int(input("Escribe un número:"))
+cantidadImpares = 0
+n = int(input("Escribe un número: "))
+
+while sumatoriaDigitos(n) <= 1000 and n%5 != 0:
+    if esImpar(n) :
+        cantidadImpares += 1
+    n = int(input("Escribe un número: "))
+
+"""
+Durante el proceso de depuración me di cuenta de que la primera definición funcionaba como se esperaba para que calcule que el número sea impar
+Posteriormente pase a la segunda definición para comprobar si funcionaba correctamente y tras depurar en la primera línea y darle un valor a la varible
+n si el total no era igual a 0 la definición de sumatoriaDigitos no funcionaría ya que el valor de total al iniciarse es 0 siempre una vez implementado
+el cambio volví a comprobar el funciojnamiento y ya hacía su función correctamente.
+Por último pase a comprobar la condiciones del while cuando probaba un número menor que 1000 el comando se finalizaba por lo tanto probé a cambiar el signo de mayor por menor
+y funcionó correctamente, también comprobe la segunda condición del while por si poní un multiplo de 3 también finalizaba, por lo que lo sustituí por un 5 para que funcione como
+lo pide el enunciado. Una vez hecho todos estos cambios el código funciona correctamente.
+Puse unicamente un punto de ruptura al inicio del comando(línea 161), porque python no permite puntos de ruptura en definiciones, y fuí pasando de linea en linea.
+"""
+
 print("Cantidad de impares:", cantidadImpares)
 
 
